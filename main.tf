79a4ef418a3de0270f478b3eb201bb51b81a1b61
Rachel Sharp <rachel@hashicorp.com>
2020-04-23 15:23:34 -0400

    Monorepo

---
+++ main.tf
@@ -0,0 +1,116 @@
+variable "prod_region" {
+  description = "This is where your EC2 instance will be deployed."
+}
+
+variable "dev_region" {
+  description = "This is where your EC2 instance will be deployed."
+}
+
+variable "dev_prefix" {
+  default = "dev"
+}
+
+variable "prod_prefix" {
+  default = "prod"
+}
+
+provider "aws" {
+  region = var.dev_region
+  alias  = "dev"
+}
+
+resource "random_pet" "petname" {
+  length    = 3
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "dev" {
+  provider = aws.dev
+  bucket   = "${var.dev_prefix}-${random_pet.petname.id}"
+  acl      = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.dev_prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "dev" {
+  provider     = aws.dev
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.dev.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
+
+resource "aws_s3_bucket" "prod" {
+  bucket = "${var.prod_prefix}-${random_pet.petname.id}"
+  acl    = "public-read"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.prod_prefix}-${random_pet.petname.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "prod" {
+  acl          = "public-read"
+  key          = "index.html"
+  bucket       = aws_s3_bucket.prod.id
+  content      = file("${path.module}/assets/index.html")
+  content_type = "text/html"
+
+}
+
+
+output "dev_website_endpoint" {
+  value = "http://${aws_s3_bucket.dev.website_endpoint}/index.html"
+}
+
+output "prod_website_endpoint" {
+  value = "http://${aws_s3_bucket.prod.website_endpoint}/index.html"
+}
